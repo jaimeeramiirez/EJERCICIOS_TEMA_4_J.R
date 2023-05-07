eeaef5b71dc34cb1c3da6c683bbcee152610055d
jaimeeramiirez <jaime.ramirezrodriguez@usp.ceu.es>
2023-05-07 19:05:39 +0200

    Update ejercicio4.py

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def f(x):
     return x**3 + x + 16
 
@@ -30,13 +32,43 @@ def df(x): #Derivada de la función para el método de Newton
     return 3 * x**2 + 1
 
 def newton(f, f_derivada, x0, tol, max_iter):
+    f_derivada = np.vectorize(f_derivada)
     contador = 0
     x1 = x0 - f(x0) / df(x0)
-    while abs(x1 - x0) > tol and iter_count < max_iter:
+    while abs(x1 - x0) > tol and contador < max_iter:
         x0 = x1
         x1 = x0 - f(x0) / df(x0)
-        iter_count += 1
-    return x1, iter_count
+        contador += 1
+    return x1, contador
+
+tol = 1e-6
+max_iter = 100
+
+
+# Encuentra un intervalo que contenga una raíz para el método de bisección
+a, b = -10, 10
+while np.sign(f(a)) == np.sign(f(b)):
+    a -= 1
+    b += 1
+
+sol_bisection, iter_bisection = biseccion(f, a, b, tol, max_iter)
+sol_secant, iter_secant = secante(f, a, b, tol, max_iter)
+sol_newton_raphson, iter_newton_raphson = newton(f, df, (a + b) / 2, tol, max_iter)
+
+print("Método\t\tCantidad de iteraciones\t       Solución")
+print(f"Bisección\t{iter_bisection}\t\t\t{sol_bisection}")
+print(f"Secante\t\t{iter_secant}\t\t\t{sol_secant}")
+print(f"Newton-Raphson\t{iter_newton_raphson}\t\t\t{sol_newton_raphson}")
+
+diff_sec_bis = abs(sol_secant - sol_bisection)
+diff_newt_bis = abs(sol_newton_raphson - sol_bisection)
+diff_newt_sec = abs(sol_newton_raphson - sol_secant)
+
+print(f"\nDiferencia en decimales:")
+print(f"Secante-Bisección: {diff_sec_bis:.6f}")
+print(f"Newton-Raphson-Bisección: {diff_newt_bis:.6f}")
+print(f"Newton-Raphson-Secante: {diff_newt_sec:.6f}")
+
 
 
 
