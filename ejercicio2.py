b69306f30de35f355d043142f6becf1ecf1b4db3
jaimeeramiirez <jaime.ramirezrodriguez@usp.ceu.es>
2023-05-07 18:38:00 +0200

    Update ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -27,3 +27,31 @@ def mostrar_recursos(misiones):
         print(f"Prioridad: {mision['prioridad']}")
         print(f"Recursos asignados: {mision['recursos']}")
         print()
+
+def main():
+    misiones = [] #Lista de misiones
+    misiones.append(asignar_recursos("exploracion", "Hoth", "Darth Vader")) #Agregamos misiones a la lista
+    misiones.append(asignar_recursos("contencion", "Endor", "Palpatine")) 
+    misiones.append(asignar_recursos("ataque", "Yavin IV", "Darth Vader"))
+    misiones.append(asignar_recursos("contencion", "Dagobah", "Tarkin"))
+    misiones.append(asignar_recursos("ataque", "Kashyyyk", "Tarkin"))
+    misiones.append(asignar_recursos("exploracion", "Mustafar", "Tarkin"))
+    misiones.append(asignar_recursos("exploracion", "Dantooine", "Palpatine"))
+    misiones.append(asignar_recursos("contencion", "Geonosis", "Tarkin"))
+    misiones.append(asignar_recursos("ataque", "Naboo", "Darth Vader"))
+    misiones.append(asignar_recursos("contencion", "Kamino", "Tarkin"))
+    misiones.append(asignar_recursos("ataque", "Coruscant", "Palpatine"))
+    misiones.append(asignar_recursos("exploracion", "Tatooine", "Darth Vader"))
+    misiones.append(asignar_recursos("exploracion", "Lothal", "Tarkin"))
+    misiones.append(asignar_recursos("contencion", "Jakku", "Palpatine"))
+    misiones.append(asignar_recursos("ataque", "Scarif", "Tarkin"))
+    misiones.append(asignar_recursos("contencion", "Bespin", "Darth Vader"))
+    misiones.append(asignar_recursos("ataque", "Alderaan", "Palpatine"))
+    misiones.append(asignar_recursos("exploracion", "Kessel", "Tarkin"))
+    misiones.append(asignar_recursos("exploracion", "Mandalore", "Darth Vader"))
+    misiones.append(asignar_recursos("contencion", "Starkiller", "Palpatine"))
+    
+    mostrar_recursos(misiones)
+
+if __name__ == "__main__":
+    main()
