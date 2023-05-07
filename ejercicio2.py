73d4b0395525c137572a08a37ff3974db791f722
jaimeeramiirez <jaime.ramirezrodriguez@usp.ceu.es>
2023-05-07 13:49:54 +0200

    Update ejercicio2.py

---
+++ ejercicio2.py
@@ -0,0 +1,7 @@
+import random
+
+def asignar_recursos(tipo, planeta_destino, general):
+    vehiculos = {
+        "contencion": ["AT-AT", "AT-RT", "AT-TE", "AT-DP", "AT-ST"],
+        "ataque": ["AT-AT", "AT-RT", "AT-TE", "AT-DP", "AT-ST", "AT-M6", "AT-MP", "AT-DT"]
+    }
