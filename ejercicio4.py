259c525a1c303dcd09970797e6a16f8ae7d6ec12
jaimeeramiirez <jaime.ramirezrodriguez@usp.ceu.es>
2023-05-07 18:56:14 +0200

    Update ejercicio4.py

--- ejercicio4.py
+++ ejercicio4.py
@@ -14,3 +14,30 @@ def biseccion(f, a, b, tol, max_iter):
             a = c
         contador += 1
     return c, contador
+
+#Función secante
+def secante(f, x0, x1, tol, max_iter):
+    contador = 0
+    while abs(x1 - x0) > tol and contador < max_iter:
+        x_new = x1 - f(x1) * (x1 - x0) / (f(x1) - f(x0))
+        x0, x1 = x1, x_new
+        contador += 1
+    return x1, contador
+
+#Función de Newton
+
+def df(x): #Derivada de la función para el método de Newton
+    return 3 * x**2 + 1
+
+def newton(f, f_derivada, x0, tol, max_iter):
+    contador = 0
+    x1 = x0 - f(x0) / df(x0)
+    while abs(x1 - x0) > tol and iter_count < max_iter:
+        x0 = x1
+        x1 = x0 - f(x0) / df(x0)
+        iter_count += 1
+    return x1, iter_count
+
+
+
+
