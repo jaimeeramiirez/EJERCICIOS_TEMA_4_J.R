3a75543c268cac68f9e96e07af0a9b0a24b4fda3
jaimeeramiirez <jaime.ramirezrodriguez@usp.ceu.es>
2023-05-07 13:32:25 +0200

    Update ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -42,21 +42,11 @@ caracter_cantidad = {
     'U': 4, 'V': 2, ' ': 17, ',': 2
 }
 
-import heapq
-from collections import defaultdict
-
-# Datos iniciales
-caracter_cantidad = {
-    'A': 11, 'B': 2, 'C': 4, 'D': 3, 'E': 14, 'G': 3, 'I': 6, 'L': 6,
-    'M': 3, 'N': 6, 'O': 7, 'P': 4, 'Q': 1, 'R': 10, 'S': 4, 'T': 3,
-    'U': 4, 'V': 2, ' ': 17, ',': 2
-}
-
 # Calcular frecuencias
-total_caracteres = sum(caracter_cantidad.values())
-frecuencias = {caracter: cantidad / total_caracteres for caracter, cantidad in caracter_cantidad.items()}
+total_caracteres = sum(caracter_cantidad.values()) 
+frecuencias = {caracter: cantidad / total_caracteres for caracter, cantidad in caracter_cantidad.items()} 
 
-# Crear árbol de Huffman
+#Arbol de Huffman
 def crear_arbol_huffman(frecuencias):
     heap = [[peso, [caracter, ""]] for caracter, peso in frecuencias.items()]
     heapq.heapify(heap)
